4441e8be062ab44626a075f892c1eab783d77f91
krenzew <54631709+krenzew@users.noreply.github.com>
2020-03-26 10:55:31 +0100

    Completed Creating a WatchOS App Tutorial

---
+++ Framework Integration/2 - CreatingAwatchOSApp/Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Eric Krenz on 3/26/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList { WatchLandmarkDetail(landmark: $0) }
+        .environmentObject(UserData())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList { WatchLandmarkDetail(landmark: $0) }
+        .environmentObject(UserData())
+    }
+}
